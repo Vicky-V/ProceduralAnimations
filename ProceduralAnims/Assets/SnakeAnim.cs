85c174570a16fdf7add4f2aff21524b5b0ccc65d
Viktoriya Voinova <voin0001@algonquinlive.com>
2016-06-29 19:57:09 -0400

    Added initial anim script for snake

---
+++ ProceduralAnims/Assets/SnakeAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SnakeAnim : MonoBehaviour 
+{
+	public List<GameObject> Bones;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		for (int i = 0; i<Bones.Count;i++)
+		{
+			float rotY = Mathf.Atan(Mathf.Cos(i*(Mathf.PI/16)+Time.time));
+			Bones[i].transform.localRotation = Quaternion.Euler(0.0f,rotY,0.0f);
+		}
+	}
+}
