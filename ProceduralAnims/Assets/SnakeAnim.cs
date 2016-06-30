fdbfb7a87ad77fa0e0e3d41c4932f5c40a3134b3
Viktoriya Voinova <voin0001@algonquinlive.com>
2016-06-29 20:31:40 -0400

    Some attempts at snake animation

--- ProceduralAnims/Assets/SnakeAnim.cs
+++ ProceduralAnims/Assets/SnakeAnim.cs
@@ -5,20 +5,20 @@ using System.Collections.Generic;
 public class SnakeAnim : MonoBehaviour 
 {
 	public List<GameObject> Bones;
+	public float waveFreq=1;
+	public float waveHeight=1;
 
-	// Use this for initialization
 	void Start () 
 	{
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () 
 	{
 		for (int i = 0; i<Bones.Count;i++)
 		{
-			float rotY = Mathf.Atan(Mathf.Cos(i*(Mathf.PI/16)+Time.time));
-			Bones[i].transform.localRotation = Quaternion.Euler(0.0f,rotY,0.0f);
+			float rotY = Mathf.Rad2Deg * Mathf.Atan(waveFreq*waveHeight*Mathf.Cos(waveFreq*i+Time.time));
+			Bones[i].transform.eulerAngles = new Vector3(0.0f,rotY,0.0f);
 		}
 	}
 }
