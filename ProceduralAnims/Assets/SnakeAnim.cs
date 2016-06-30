9a780b0313d02fc20e1a1eddd2888367601b4745
Viktoriya Voinova <voin0001@algonquinlive.com>
2016-06-29 22:02:08 -0400

    Finished snake animation!

--- ProceduralAnims/Assets/SnakeAnim.cs
+++ ProceduralAnims/Assets/SnakeAnim.cs
@@ -7,6 +7,8 @@ public class SnakeAnim : MonoBehaviour
 	public List<GameObject> Bones;
 	public float waveFreq=1;
 	public float waveHeight=1;
+	[Range (0.01f,3)]
+	public float speed = 1;
 
 	void Start () 
 	{
@@ -15,9 +17,12 @@ public class SnakeAnim : MonoBehaviour
 
 	void Update () 
 	{
+
+		float timeStep = Time.time / (1/speed);
+
 		for (int i = 0; i<Bones.Count;i++)
 		{
-			float rotY = Mathf.Rad2Deg * Mathf.Atan(waveFreq*waveHeight*Mathf.Cos(waveFreq*i+Time.time));
+			float rotY = Mathf.Rad2Deg * Mathf.Atan(waveFreq*waveHeight*Mathf.Cos(waveFreq*i-timeStep));
 			Bones[i].transform.eulerAngles = new Vector3(0.0f,rotY,0.0f);
 		}
 	}
